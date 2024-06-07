3d8eb8b85de751137eee6d022d19dbbb60ceff5c
chox7 <chorzelakacper@gmail.com>
2024-06-07 02:44:45 +0200

    Usunięto z maina niepotrzebne rzeczy.

--- main.py
+++ main.py
@@ -5,18 +5,14 @@ import threading
 import test_signal as ts
 
 def main():
-    #path = 'dummy_data_120s.hdf'
-    #processing_chunk_size = 1024
-    #chosen_channels = [23, 24, 25, 26, 27, 28, 29]
+    path = 'dummy_data_120s.hdf'
+    processing_chunk_size = 1024
+    chosen_channels = [23, 24, 25, 26, 27, 28, 29]
     #data = lsl.simulate_aquisition(path, processing_chunk_size)
 
-    Fs = 2048
-    n_ch = 3
-    s_path = 'wysilek.obci.raw'
-
-    data = ts.test_signal(s_path, n_ch, Fs)
+    data = ts.test_signal()
     filts = lsl.initialize_filters(500)
-    HR = ekgp.HRProcessor(sampling_rate=Fs, window_size=2)
+    HR = ekgp.HRVProcessor(sampling_rate=2048, window_size=1)
 
     data_thread = threading.Thread(target=ekgapp.add_data_continuously, args=(HR, data, filts))
     data_thread.start()

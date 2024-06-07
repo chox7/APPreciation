9531d1a1bf759000f1751d5c87d76314c12191a8
chox7 <chorzelakacper@gmail.com>
2024-06-07 02:44:01 +0200

    Dodano wykres tętna do gui.

--- EKGapp.py
+++ EKGapp.py
@@ -25,8 +25,9 @@ def add_data_continuously(HR, data, filts):
 def run_dash_app(processor):
     app = dash.Dash(__name__)
     app.layout = html.Div([
-        dcc.Graph(id='live-update-graph'),
-        dcc.Graph(id='breathing-scheme', figure=creating_ramp()),
+        dcc.Graph(id='live-graph-ekg', style={'width': '66%', 'display': 'inline-block'}),
+        dcc.Graph(id='live-graph-hr', style={'width': '33%', 'display': 'inline-block'}),
+        dcc.Graph(id='breathing-scheme', figure=creating_ramp(info_from_user = False)),
         dcc.Interval(
             id='interval-component',
             interval=50,  # Aktualizacja co 50 ms
@@ -35,24 +36,76 @@ def run_dash_app(processor):
     ])
 
     @app.callback(
-        Output('live-update-graph', 'figure'),
+        Output('live-graph-ekg', 'figure'),
         [Input('interval-component', 'n_intervals')]
     )
     def update_EKG_plot(n):
-        data_buffer = processor.get_data()
-        print(data_buffer.mean())
+        data_buffer, time_buffer = processor.get_data()
+
         ekg_trace = go.Scatter(
+            x=time_buffer,
             y=data_buffer,
             mode='lines',
-            name='EKG Signal'
+            name=f'EKG Signal',
         )
+        
+        peaks, prominences = processor.get_peaks()
+        shapes = []
+        for peak, prominence in zip(peaks, prominences):
+            shapes.append(
+                dict(
+                    type="line",
+                    x0=peak, y0=-200,#data_buffer[int((peak - time_buffer[0])*processor.sampling_rate)] - prominence,
+                    x1=peak, y1=200,#data_buffer[int((peak - time_buffer[0])*processor.sampling_rate)],
+                    line=dict(color="orange", width=2)
+                )
+            )
+        
 
         return {
             'data': [ekg_trace],
             'layout': go.Layout(
-                title='Live EKG Data',
-                xaxis=dict(), #TODO: Dodać czas na osi x
-                yaxis=dict(range=[0.8 * np.min(data_buffer), 1.2 * np.max(data_buffer)])  # Dostosuj zakres osi Y do swoich danych
+                title=f'Live EKG Data',
+                shapes=shapes,
+                plot_bgcolor='white',  # Białe tło wykresu
+                paper_bgcolor='white',  # Białe tło papieru
+                xaxis=dict(
+                    gridcolor='white',  # Siatka w kolorze jasnoszarym
+                    linecolor='white',  # Linia osi X w kolorze czarnym
+                    range=[time_buffer[0], time_buffer[-1]]
+                ),
+                yaxis=dict(
+                    gridcolor='lightgrey',  # Siatka w kolorze jasnoszarym
+                    linecolor='black',  # Linia osi Y w kolorze czarnym
+                    range=[1.2 * np.min(data_buffer), 1.2 * np.max(data_buffer)]
+                )
+            )
+        }
+    @app.callback(Output('live-graph-hr', 'figure'),
+              Input('interval-component', 'n_intervals'))
+    def update_HR_plot(n):
+        bpm = processor.get_bpm()
+        hr_trace = go.Scatter(
+            y=bpm,
+            mode='lines',
+            name='Heart Rate'
+        )
+        
+        return {
+            'data': [hr_trace],
+            'layout': go.Layout(
+                title='Live Heart Rate',
+                plot_bgcolor='white',  # Białe tło wykresu
+                paper_bgcolor='white',  # Białe tło papieru
+                xaxis=dict(
+                    gridcolor='lightgrey',  # Siatka w kolorze jasnoszarym
+                    linecolor='black'  # Linia osi X w kolorze czarnym
+                ),
+                yaxis=dict(
+                    gridcolor='lightgrey',  # Siatka w kolorze jasnoszarym
+                    linecolor='black',  # Linia osi Y w kolorze czarnym
+                    range=[20,300]  # Zakres tętna w BPM (dostosuj do potrzeb)
+                )
             )
         }
     return app

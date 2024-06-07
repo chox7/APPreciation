dabe8ddea8a3b781897c39ee39642c74e008db73
chox7 <chorzelakacper@gmail.com>
2024-06-07 02:45:28 +0200

    Przeniesiono z main parametry dla testowego sygnału.

--- test_signal.py
+++ test_signal.py
@@ -3,10 +3,18 @@ import time
 
 def signal_generator(s):
     for i in range(0, len(s[:,0]), 16):
-        time.sleep(0.05)
-        yield s[i:i+16, :]
+        time.sleep(0.005)
+        # Zakładam, że:
+        # kanał 2 posiada sygnał EKG z lewej nogi
+        # kanał 1 posiada sygnał EKG z prawej kończyny górnej (nadgarstek lub ramię)
+        # EINTHOVEN II = VF - VR
+        #eint_2 = -1 * new_data[:,23]
+        eint_2 = s[:, 2] - s[:, 1]
+        yield eint_2[i:i+16]
 
-def test_signal(s_path, n_ch, Fs):
+def test_signal():
+    n_ch = 3
+    s_path = 'wysilek.obci.raw'
     s = np.fromfile(s_path, dtype='<f')
     s = s * 0.0715
     s = np.reshape(s, (len(s)//n_ch, n_ch))

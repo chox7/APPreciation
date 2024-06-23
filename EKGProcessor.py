70675005bf3c6efc2cf7c1e90004e0e1029f13dd
natistepien2003 <natistepien2003@gmail.com>
2024-06-23 18:32:18 +0200

    adding coherence

--- EKGProcessor.py
+++ EKGProcessor.py
@@ -4,7 +4,7 @@ from scipy.signal import find_peaks
 from scipy import interpolate
 from periodogram import periodogram
 from scipy.signal import windows
-
+from scipy import integrate
 
 class HRVProcessor:
     def __init__(self, sampling_rate=500, window_size=5):
@@ -15,11 +15,12 @@ class HRVProcessor:
         self.time_buffer = np.linspace(0, window_size, sampling_rate * window_size)
         self.last_peak_index = -1 # index of the last peak in the data_buffer in seconds
         self.peaks_time = deque(maxlen=21)
-        self.peaks_prominence = deque(maxlen=20)
+        self.peaks_prominence = deque(maxlen=10)
         self.bpm_list = deque(maxlen=20)
         self.time_list = deque(maxlen=20)
-        self.frequencies_list = deque(maxlen=20)
-        self.power_list = deque(maxlen=20)
+        self.frequencies = deque(maxlen=20)
+        self.power = deque(maxlen=20)
+        self.coherence = deque(maxlen=1000)
 
     def add_data(self, new_data):
         self.data_buffer.extend(new_data)
@@ -50,6 +51,7 @@ class HRVProcessor:
                 self.rr_intervals.extend(rr_interval)
                 self.calculate_bpm()
                 self.calculate_hrv()
+                self.calculate_coherence()
                 
     def calculate_bpm(self):
         if len(self.rr_intervals) < 1:
@@ -59,7 +61,7 @@ class HRVProcessor:
         bpm = int(60.0 / avg_rr_interval)
         self.bpm_list.extend([bpm])
 
-        #potrzebne do hrv
+        #hrv
         time = [self.peaks_time[i]/500 for i in range(len(self.peaks_time)-1)]
         self.time_list = time
 
@@ -86,11 +88,30 @@ class HRVProcessor:
 
         okno = windows.hann(len(time))
         (F, P) = periodogram(sig, okno, 1)    
-        self.frequencies_list = F
-        self.power_list = P
+        self.frequencies = F
+        self.power = P
 
     def get_frequencies(self):
-        return np.array(self.frequencies_list)
+        return np.array(self.frequencies)
         
     def get_power(self):
-        return np.array(self.power_list)
+        return np.array(self.power)
+    
+
+    ''' frequencies: [0.   0.05 0.1  0.15 0.2  0.25 0.3  0.35 0.4  0.45 0.5 ] '''
+    
+    def calculate_coherence(self):
+        if len(self.frequencies) < 9:
+            return
+        
+        total_power = integrate.simps(self.frequencies[1:-2])
+        highest_peak_index = np.argmax(self.frequencies[1:6])
+        peak_power = integrate.simps(self.frequencies[(highest_peak_index-1):(highest_peak_index+1)])
+        coherence_value  = (peak_power/(total_power-peak_power))**2
+        sigma = 1
+        mu=0
+        x = np.linspace(mu - 4*sigma, mu + 4*sigma, 1000)
+        self.coherence = ((1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-((x - mu)**2) / (2 * sigma**2)))/0.4*coherence_value
+
+    def get_coherence(self):
+        return np.array(self.coherence)

75dfc0d27e31c3b9188b35524b7af65c3bb7cffe
Kacper Chorzela <chorzelakacper@gmail.com>
2024-07-01 23:54:17 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,14 @@ import threading
 import test_signal as ts
 
 def main():
-    #path = 'dummy_data_120s.hdf'
-    #processing_chunk_size = 256
-    #chosen_channels = [23, 24, 25, 26, 27, 28, 29]
-    #data = lsl.simulate_aquisition(path, processing_chunk_size)
-
     inlet = lsl.start_stream('stream_1', 500)
     Fs = 500
-    filts = lsl.initialize_filters(Fs)
+
     processor = ekgp.SignalProcessor(sampling_rate=Fs, buffor_size_seconds=5)
     peaks_detector =  ekgp.PeaksDetector(processor)
     hrv_analyzer = ekgp.HRVAnalyzer(peaks_detector)
 
-    data_thread = threading.Thread(target=ekgapp.add_data_continuously, args=(processor, data, filts))
+    data_thread = threading.Thread(target=ekgapp.add_data_continuously, args=(inlet, 500, processor))
     data_thread.start()
 
     ekgapp.run_dash_app_thread(processor, peaks_detector, hrv_analyzer)
